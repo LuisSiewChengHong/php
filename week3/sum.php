c020e2d8ad651f12c7bfc6f992c05c3255478914
Luis Siew <luissiew0411@e.neivce.edu.my>
2024-11-20 15:21:20 +0800

    Sum of 1-10 (For Loop)

---
+++ week3/sum.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            
+        </style>
+    </head>
+<body>
+<?php
+
+$TotalSum = 0;
+
+for($i = 0; $i <= 10; $i++)
+{
+    $TotalSum += $i;
+    echo "Numbers: $i <br>";
+}
+
+echo $TotalSum;
+
+
+
+
+?>
+</body>
+</html>
