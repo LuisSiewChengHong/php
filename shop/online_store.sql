80263addb47a7665c099128c0712cd071add0935
Luis Siew <luissiew0411@e.neivce.edu.my>
2025-01-22 12:41:41 +0800

    newPhpAdmin

--- shop/online_store.sql
+++ shop/online_store.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jan 15, 2025 at 06:12 AM
+-- Generation Time: Jan 22, 2025 at 04:41 AM
 -- Server version: 8.3.0
 -- PHP Version: 8.2.18
 
@@ -31,7 +31,7 @@ DROP TABLE IF EXISTS `customer`;
 CREATE TABLE IF NOT EXISTS `customer` (
   `username` varchar(128) NOT NULL,
   `password` int NOT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT '1',
+  `stat` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`username`) USING BTREE
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -39,7 +39,7 @@ CREATE TABLE IF NOT EXISTS `customer` (
 -- Dumping data for table `customer`
 --
 
-INSERT INTO `customer` (`username`, `password`, `status`) VALUES
+INSERT INTO `customer` (`username`, `password`, `stat`) VALUES
 ('luis', 1234, 1);
 
 -- --------------------------------------------------------
@@ -50,15 +50,25 @@ INSERT INTO `customer` (`username`, `password`, `status`) VALUES
 
 DROP TABLE IF EXISTS `products`;
 CREATE TABLE IF NOT EXISTS `products` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `description` text NOT NULL,
   `product_cat` int NOT NULL,
   `price` double NOT NULL,
+  `manufacture_date` datetime NOT NULL,
+  `expired_date` datetime NOT NULL,
   `created` datetime NOT NULL,
   `modified` timestamp NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `description`, `product_cat`, `price`, `manufacture_date`, `expired_date`, `created`, `modified`) VALUES
+(1, 'RTX 5070', 'Graphic Card', 3, 500, '2025-01-01 00:00:00', '2025-01-19 00:00:00', '2025-01-15 08:41:29', '0000-00-00 00:00:00'),
+(7, 'Sofa', 'A comfy seat to be sat on.', 1, 400, '2025-01-02 00:00:00', '2025-01-08 00:00:00', '2025-01-15 09:33:52', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -68,11 +78,11 @@ CREATE TABLE IF NOT EXISTS `products` (
 
 DROP TABLE IF EXISTS `product_cat`;
 CREATE TABLE IF NOT EXISTS `product_cat` (
-  `product_cat_id` int NOT NULL,
+  `product_cat_id` int NOT NULL AUTO_INCREMENT,
   `product_cat_name` varchar(50) NOT NULL,
   `product_cat_description` text NOT NULL,
   PRIMARY KEY (`product_cat_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `product_cat`
